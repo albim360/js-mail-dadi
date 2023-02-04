87929911fc453f7820721f9fa54e44ad31348217
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-04 01:37:59 +0100

    FEAT abbellito con css e allegerito codice js

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -1,27 +1,19 @@
 function playGame() {
-    // Genera i numeri casuali per il giocatore e il computer
-    let playerNumber = Math.floor(Math.random() * 6) + 1;
-    let computerNumber = Math.floor(Math.random() * 6) + 1;
-    
-    console.log("Numero del giocatore:", playerNumber);
-    console.log("Numero del computer:", computerNumber);
-    
-    // Crea un array per memorizzare i punteggi
-    let scores = [playerNumber, computerNumber];
-    console.log("Punteggi:", scores);
-    
-    // Confronta i numeri per stabilire il vincitore
-    let winner;
-    if (playerNumber > computerNumber) {
-      winner = "il Giocatore";
-    } else if (computerNumber > playerNumber) {
-      winner = "il Computer";
-    } else {
-      winner = "Nessuno, è un pareggio";
-    }
-    
-    console.log("Vincitore:", winner);
-    
-    // Stampa il risultato
-    alert("Il giocatore ha totalizzato " + scores[0] + ", il computer ha totalizzato " + scores[1] + ". Il vincitore è: " + winner + ".");
+  // Genera i numeri casuali per il giocatore e il computer
+  let playerNumber = Math.floor(Math.random() * 6) + 1;
+  let computerNumber = Math.floor(Math.random() * 6) + 1;
+
+  // Confronta i numeri per stabilire il vincitore
+  let winner;
+  if (playerNumber > computerNumber) {
+    winner = "Vince il Giocatore";
+  } else if (computerNumber > playerNumber) {
+    winner = "Vince il Computer";
+  } else {
+    winner = "Pareggio";
   }
+
+  // Mostra il risultato
+  let resultEl = document.getElementById("result");
+  resultEl.innerHTML = `Il giocatore ha totalizzato ${playerNumber}, il computer ha totalizzato ${computerNumber}. : ${winner}.`;
+}
