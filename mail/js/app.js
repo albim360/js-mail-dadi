4b1a53910df3e75c0da2350f42a04e5b0b53635c
Alberto <74522184+albim360@users.noreply.github.com>
2023-02-04 02:23:45 +0100

    FEAT aggiunto stile e sistemato codice js

--- mail/js/app.js
+++ mail/js/app.js
@@ -1,27 +1,36 @@
-// Definisci l'array di email
-const emailList = ["email1@icloud.com", "email2@gmail.com", "email3@libero.com", "tiziocaio@email.it"];console.log(emailList)
-emailList.push("email4@hotmail.com", "email5@yahoo.com");
+// Definiamo l'array di email
+const emailList = ["email5@yahoo.com", "email4@hotmail.com", "email3@libero.com", "tiziocaio@email.it", "email2@gmail.com", "email1@icloud.com"];
 
-console.log(emailList)
+// Aggiungiamo altre email alla lista
+emailList.push("email12@hotmail.com", "email11@yahoo.com");
 
+// Funzione per controllare l'email dell'utente
 function checkEmail() {
-  // Chiedi l'email all'utente
-  const userEmail = prompt("Inserisci la tua email:");
-  
-  // Controlla se l'email dell'utente è nella lista
-  let emailFound = false;
-  for (let i = 0; i < emailList.length; i++) {
-    if (userEmail === emailList[i]) {
-      emailFound = true;
-      break;
-    }
-    console.log(i)
-  }
-  
-  // Stampa il risultato
-  if (emailFound) {
-    alert("Email trovata nella lista");
-  } else {
-    alert("Email non trovata nella lista");
-  }
+// Prendiamo la email inserita dall'utente
+const userEmail = document.getElementById("userEmail").value;
+
+// Verifichiamo se l'email dell'utente è nella lista
+let emailFound = false;
+for (let i = 0; i < emailList.length; i++) {
+if (userEmail === emailList[i]) {
+emailFound = true;
+break;
 }
+}
+
+// Mostriamo il risultato
+const resultContainer = document.getElementById("resultContainer");
+resultContainer.innerHTML = emailFound ? "Email " + userEmail + " trovata nella lista" : "Email " + userEmail + " non trovata nella lista";
+resultContainer.classList.add(emailFound ? "success" : "fail");
+
+// Fai scomparire il risultato dopo 5 secondi
+setTimeout(function() {
+    resultContainer.innerHTML = "";
+    resultContainer.className = "";
+}, 2500);
+
+
+}
+
+// Aggiungiamo l'evento click al bottone "Controlla email"
+document.getElementById("checkEmailButton").addEventListener("click", checkEmail);
